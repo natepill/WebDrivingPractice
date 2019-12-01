268f80a8533a21f94ee08530f5f62c25d90f8955
Nathan Pillai <natepill@gmail.com>
2019-11-30 17:54:23 -0800

    [added] driver setup and init testing

---
+++ app.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome('/Users/natepill/dev/webdriver-practice/chromedriver')
+
+
+browser.get("https://www.youtube.com/watch?v=_uk_6vfqwTA")
+
+
+
+dropdown-trigger style-scope ytd-menu-renderer
+
+More actions
