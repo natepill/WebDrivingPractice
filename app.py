90970d9d2dcaf6bffe6731d5953b2e921d6c45d3
Nathan Pillai <natepill@gmail.com>
2019-11-30 18:18:57 -0800

    [debugged] method naming for driver element

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ browser = webdriver.Chrome('/Users/natepill/dev/webdriver-practice/chromedriver'
 
 browser.get("https://www.youtube.com/watch?v=_uk_6vfqwTA")
 
+more_actions_bt = browser.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button")
 
+# dropdown-trigger style-scope ytd-menu-renderer
+print(more_actions_bt)
+more_actions_bt.click()
 
-dropdown-trigger style-scope ytd-menu-renderer
-
-More actions
+# More actions
